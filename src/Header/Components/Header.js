b5c1d7468514922c6eef13bd3ba3f1694291eb50
karagudisuma <ksumaudupa@gmail.com>
2019-03-09 16:32:32 +0530

    Header improvement

--- src/Header/Components/Header.js
+++ src/Header/Components/Header.js
@@ -7,21 +7,30 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        console.log("clicked");
+    }
+
     render() {
         return (
-            <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">SHOPMATE</Navbar.Brand>
+            <Navbar className="dark" bg="dark" expand="lg" variant="dark">
+            <Navbar.Brand className="logo">SHOPMATE</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Women</Nav.Link>
-                <Nav.Link href="#link">Men</Nav.Link>
-                <Nav.Link href="#home">Kids</Nav.Link>      
-                <Nav.Link href="#link">Shoes</Nav.Link>
-                <Nav.Link href="#link">Brand</Nav.Link>
+                <Nav className="nav-fill w-100">
+                <Nav.Link onClick={this.handleClick}>Women</Nav.Link>
+                <Nav.Link>Men</Nav.Link>
+                <Nav.Link>Kids</Nav.Link>      
+                <Nav.Link>Shoes</Nav.Link>
+                <Nav.Link>Brand</Nav.Link>
                 </Nav>
                 <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <FormControl type="text" placeholder="Search Anything" className="mr-2 pl-2" bsPrefix="form-control-search"/>
                 </Form>
             </Navbar.Collapse>
             </Navbar>
