c2f6ddd18ee253ca299397d1c48bd2f79b5947fc
karagudisuma <ksumaudupa@gmail.com>
2019-03-10 00:52:01 +0530

    Home page - bottom section commit

--- src/Home/Components/Home.js
+++ src/Home/Components/Home.js
@@ -1,8 +1,49 @@
 import React, { Fragment, Component } from 'react';
 import '../Styles/style.scss';
 import Image from 'react-bootstrap/Image';
-import headerImg from './Images/hero-1@3x.png';
+import Button from 'react-bootstrap/Button';
+import headerImg from '../Images/hero-1@3x.png';
+import toteBagImg from '../Images/totebag.jpg';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 import { withErrorBoundary } from '../../ErrorPage';
+
+function TotoBagComponent() {
+    return (
+        <div class="tote-container">
+            <div className="mx-2 tote-img"><img src={toteBagImg} width="280" height="280"></img></div>
+            <div className="mx-3 tote-text">
+                <h2 className="bag-heading">Vera Bradley</h2>
+                <p className="para-heading">Carry the day in style with this extra-large tote crafted in our chic B.B. Collection textured PVC. Featuring colorful faux leather trim, this tote offers a roomy interior plus just enough perfectly placed pockets to keep smaller items organized and easy to find.</p>
+                <Button variant="primary" bsPrefix="btn-shop">Shop now</Button>
+            </div>    
+        </div>
+
+    );
+}
+
+function WowComponent(){
+    return (
+        <div className="wow-container">
+            <p className="wow-heading">wow</p>
+            <p className="wow-text-one">Check</p>
+            <p className="wow-text-two">What!</p>
+        </div>    
+    )
+}
+
+function BottomComponent(){
+    return (
+        <div class="bottom-container p-5">
+            <p className="btm-disc-text">10% Discount for your subscription</p>
+            <p className="btm-desc-txt">Carry the day in style with this extra-large tote crafted in our chic B.B. Collection textured PVC. Featuring colorful faux leather trim, this tote offers a roomy interior.</p>
+            <Form inline>
+                <FormControl type="text" placeholder="Your email here" className="m-2 p-2" bsPrefix="form-control-email"/>
+                <Button variant="primary" bsPrefix="btn-subscribe">Subscribe</Button>
+            </Form>
+        </div>    
+    )
+}
 
 class Home extends Component {
     constructor(props){
@@ -10,14 +51,16 @@ class Home extends Component {
     }
     render(){
         return (
+            <Fragment>
             <div className="home-container">
-                <div className="home-main"><Image src={headerImg} fluid />;</div>
-                <div className="home-second-section">Vera Bradley</div>
-                <div className="home-side-one">Wow</div>
+                <div className="home-main"><Image src={headerImg} fluid /></div>
+                <div className="home-second-section"><TotoBagComponent /></div>
+                <div className="home-side-one"><WowComponent /></div>
                 <div className="home-side-two">Men</div>
                 <div className="home-side-three">Register</div>
-                <div className="home-bottom-section">Bottom</div>
-            </div>    
+                <div className="home-bottom-section"><BottomComponent /></div>
+            </div>  
+            </Fragment>  
         )
     }
 }
